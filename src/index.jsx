317cc256e54c42f73c4d491294144931440241c2
ilya <theilyashulepov@gmail.com>
2024-10-02 21:00:55 +0300

    e2e playwright

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('app')).render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <App /> }>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
