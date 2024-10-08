1eaf4ccaf81e5a111520cc1f8e7d82a52ac3823a
ilya <theilyashulepov@gmail.com>
2024-10-08 17:44:39 +0300

    add /version

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure new version is deployed
+});
+
 app.listen(PORT, () => {
   /* eslint-disable */
   console.log(`server started on port ${PORT}`);
